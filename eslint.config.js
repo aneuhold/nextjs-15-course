42d58175482819e49c470cd29f8770f266699c2f
Anton G Neuhold Jr <agneuhold@gmail.com>
2025-08-31 14:01:41 -0700

    Fix up linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-import reactConfig from '@aneuhold/eslint-config/src/react-config.js';
+import reactConfig from '@aneuhold/eslint-config/src/react-next-config.js';
 
 /** @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.ConfigArray} */
 export default [
@@ -6,10 +6,7 @@ export default [
   {
     // other override settings. e.g. for `files: ['**/*.test.*']`
     files: ['**/*.ts', '**/*.tsx'],
-    rules: {
-      // This should probably be added to the base. Not a big fan of this rule.
-      '@typescript-eslint/restrict-template-expressions': 'off',
-    },
+    rules: {},
   },
   {
     ignores: ['.next/**'],
